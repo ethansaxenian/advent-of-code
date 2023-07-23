fd03db2e1169de7941abfe54204061ebfbf60f97
Ethan Saxenian <ethansaxenian+github@proton.me>
2023-07-23 01:08:18 -0400

    2020 day 1

---
+++ 2020/util/util.go
@@ -0,0 +1,82 @@
+package util
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+)
+
+func Run[T comparable](part1, part2 func() T) {
+	part := "1"
+	if len(os.Args) >= 2 {
+		part = os.Args[1]
+	}
+
+	var ans any
+	if part == "1" {
+		ans = part1()
+	} else {
+		ans = part2()
+	}
+
+	fmt.Println(ans)
+}
+
+type session struct {
+	Cookie string `json:"aoc_cookie"`
+}
+
+func FetchInput(day int) []string {
+	_, filename, _, _ := runtime.Caller(0)
+	cookiePath := filepath.Join(filename, "../../../aoc-cookie.json")
+	data, err := os.ReadFile(cookiePath)
+	if err != nil {
+		panic(err)
+	}
+
+	url := fmt.Sprintf("https://adventofcode.com/2020/day/%d/input", day)
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	var session session
+
+	err = json.Unmarshal(data, &session)
+	if err != nil {
+		panic(err)
+	}
+
+	cookieHeader := fmt.Sprintf("session=%s", session.Cookie)
+	req.Header.Add("cookie", cookieHeader)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(string(body), "\n")
+	return lines[:len(lines)-1]
+}
+
+func ToInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+
+	return i
+}

032f76dd7920466009783487f1c1907df4a072f1
Ethan Saxenian <ethansaxenian+github@proton.me>
2024-12-07 17:31:03 -0500

    rtl

--- 2024/7.py
+++ 2024/7.py
@@ -1,23 +1,43 @@
-import operator
 from collections.abc import Callable
 
 import util
 
 
+def unmultiply(x: int, y: int) -> int:
+    if x % y:
+        raise ValueError
+    return x // y
+
+
+def unadd(x: int, y: int) -> int:
+    return x - y
+
+
+def unconcat(x: int, y: int) -> int:
+    a, b = str(x), str(y)
+    if not a.endswith(b):
+        raise ValueError
+    return int(a.removesuffix(b) or 0)
+
+
 def dfs(test: int, nums: list[int], operators: list[Callable[[int, int], int]]) -> bool:
-    stack = [(nums[0], 1)]
+    stack = [(test, len(nums) - 1)]
     while stack:
         res, i = stack.pop()
-        if res > test:
+        n = nums[i]
+        if res < 0:
             continue
 
-        if i == len(nums):
-            if res == test:
+        if i == 0:
+            if res == n:
                 return True
             continue
 
         for op in operators:
-            stack.append((op(res, nums[i]), i + 1))
+            try:
+                stack.append((op(res, n), i - 1))
+            except ValueError:
+                continue
 
     return False
 
@@ -35,14 +55,11 @@ def calibrate(lines: list[str], operators: list[Callable[[int, int], int]]) -> i
 
 
 def part1(input: str) -> int:
-    return calibrate(input.splitlines(), [operator.add, operator.mul])
+    return calibrate(input.splitlines(), [unadd, unmultiply])
 
 
 def part2(input: str) -> int:
-    return calibrate(
-        input.splitlines(),
-        [operator.add, operator.mul, lambda x, y: int(str(x) + str(y))],
-    )
+    return calibrate(input.splitlines(), [unadd, unmultiply, unconcat])
 
 
 if __name__ == "__main__":
